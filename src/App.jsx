bd39af7b7f62f021bded8777749fafe201fd03dd
sai <sairavi4304@gmail.com>
2026-05-17 21:04:03 -0500

    Set up React Router and app layout with styling

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useRoutes } from 'react-router-dom'
+import ShowCreators from './pages/ShowCreators'
+import ViewCreator from './pages/ViewCreator'
+import EditCreator from './pages/EditCreator'
+import AddCreator from './pages/AddCreator'
+import './App.css'
+
+function App() {
+  const element = useRoutes([
+    { path: '/', element: <ShowCreators /> },
+    { path: '/creator/:id', element: <ViewCreator /> },
+    { path: '/edit/:id', element: <EditCreator /> },
+    { path: '/add', element: <AddCreator /> },
+  ])
+  return element
+}
+
+export default App
